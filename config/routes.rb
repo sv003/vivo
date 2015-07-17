f8beaa529151508e83c16c3e27fd7151f5abf8b6
Mohsin Khan <mohsin.khan@consultadd.in>
2015-07-17 11:11:17 -0700

    customization still remaining

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Vivo::Application.routes.draw do
   root "pages#home"
-  get  "about" => "pages#about"
+  get  "about" => "pages#about" # about path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
